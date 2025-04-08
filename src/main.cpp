4d8435fee52984ac2764995f63b3e01e813ed666
jmuzy <jmuzyka4@gmail.com>
2025-04-08 15:41:58 -0400

    Multi Sampling

--- src/main.cpp
+++ src/main.cpp
@@ -286,7 +286,7 @@ int main(void){
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_SAMPLES, 64);
+    glfwWindowHint(GLFW_SAMPLES, 8);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     int windowWidth = 1200, windowHeight = 800;
